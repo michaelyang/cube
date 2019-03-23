e4681021651310ef6c4d38643913676cdf306f2a
Michael Yang <myang@Michael-Yang.cable.rcn.com>
2019-03-23 15:29:54 -0400

    updating cube slot and clue for 3

--- cubeData.js
+++ cubeData.js
@@ -47,73 +47,111 @@ const cubeData = [
     },
     {
         id: "2",
-        position: [0, 1, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "3",
-        position: [1, 1, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "4",
-        position: [-1, 0, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "5",
-        position: [0, 0, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "6",
-        position: [1, 0, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "7",
-        position: [-1, -1, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "8",
-        position: [0, -1, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "9",
-        position: [1, -1, 1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "10",
         position: [-1, 1, 0],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "11",
+        id: "3",
+        position: [-1, 1, -1],
+        materialsList: [
+            null,
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-b.png",
+                rotation: 180
+            },
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-u.png",
+                rotation: -90
+            },
+            null,
+            null,
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-r.png",
+                rotation: 180
+            }
+        ],
+        selectedMaterialsList: [
+            null,
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-b_selected.png",
+                rotation: 180
+            },
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-u_selected.png",
+                rotation: -90
+            },
+            null,
+            null,
+            {
+                image:
+                    "https://raw.githubusercontent.com/michaelyang/cube/master/img/3-r_selected.png",
+                rotation: 180
+            }
+        ]
+    },
+    {
+        id: "4",
+        position: [0, 1, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "5",
         position: [0, 1, 0],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "12",
+        id: "6",
+        position: [0, 1, -1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "7",
+        position: [1, 1, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "8",
         position: [1, 1, 0],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "13",
+        id: "9",
+        position: [1, 1, -1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "10",
+        position: [-1, 0, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "11",
         position: [-1, 0, 0],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "12",
+        position: [-1, 0, -1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "13",
+        position: [0, 0, 1],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
@@ -125,73 +163,73 @@ const cubeData = [
     },
     {
         id: "15",
-        position: [1, 0, 0],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "16",
-        position: [-1, -1, 0],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "17",
-        position: [0, -1, 0],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "18",
-        position: [1, -1, 0],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "19",
-        position: [-1, 1, -1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "20",
-        position: [0, 1, -1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "21",
-        position: [1, 1, -1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "22",
-        position: [-1, 0, -1],
-        materialsList: emptyMaterialsList,
-        selectedMaterialsList: emptyMaterialsList
-    },
-    {
-        id: "23",
         position: [0, 0, -1],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "24",
+        id: "16",
+        position: [1, 0, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "17",
+        position: [1, 0, 0],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "18",
         position: [1, 0, -1],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "25",
+        id: "19",
+        position: [-1, -1, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "20",
+        position: [-1, -1, 0],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "21",
         position: [-1, -1, -1],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
     {
-        id: "26",
+        id: "22",
+        position: [0, -1, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "23",
+        position: [0, -1, 0],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "24",
         position: [0, -1, -1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "25",
+        position: [1, -1, 1],
+        materialsList: emptyMaterialsList,
+        selectedMaterialsList: emptyMaterialsList
+    },
+    {
+        id: "26",
+        position: [1, -1, 0],
         materialsList: emptyMaterialsList,
         selectedMaterialsList: emptyMaterialsList
     },
