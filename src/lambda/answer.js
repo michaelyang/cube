b2b14cd564b622f831e68246c2c2eb93c4a386b9
myang <myopia92@gmail.com>
2019-03-29 15:04:59 -0400

    bug fixin

--- src/lambda/answer.js
+++ src/lambda/answer.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 exports.handler = async function(event, context, callback) {
     const hashedAnswer = await bcrypt.hash('answer', 10);
-    const answerMatch = await bcrypt.compare(hashedAnswer, ANSWER_CUBE_1);
+    const answerMatch = await bcrypt.compare('answer', ANSWER_CUBE_1);
     if (answerMatch) {
         callback(null, {
             statusCode: 200,
