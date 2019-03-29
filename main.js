9f9d20b4b2224ee9a700f0b0542e8150c4b4c159
myang <myopia92@gmail.com>
2019-03-29 16:20:20 -0400

    fixing bug

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let headers = {
 
 function handleClick(cubeID) {
     let answer = prompt(`Answer for cube ${cubeID}?`);
-    fetch(url, {
+    fetch(targetURL, {
         method: 'GET',
         headers: headers,
         body: JSON.stringify({ answer }),
