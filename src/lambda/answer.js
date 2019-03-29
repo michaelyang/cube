d93c86a4c79cfb3a1d1a0bab3ad36e804c955136
myang <myopia92@gmail.com>
2019-03-29 14:59:41 -0400

    testing env var

--- src/lambda/answer.js
+++ src/lambda/answer.js
@@ -1,6 +1,15 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'Hello, World',
-    });
+const ANSWER_CUBE_1 = process.env.ANSWER_CUBE_1;
+const bcrypt = require('bcryptjs');
+
+exports.handler = async function(event, context, callback) {
+    const hashedAnswer = await bcrypt.hash('answer', 10);
+    const answerMatch = await bcrypt.compare(hashedAnswer, ANSWER_CUBE_1);
+    if (answerMatch) {
+        callback(null, {
+            statusCode: 200,
+            body: `${hashedAnswer}`,
+        });
+    } else {
+        callback('Oh no! An error.');
+    }
 };
